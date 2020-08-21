1c704b8fe8d64c8e2081c67fdb81c9ba55a3d44b
Tobias <tobias.helsing@futurice.com>
2020-08-21 22:13:30 +0300

    refactor frag

--- src/pages/ImageProcessingMouseEffect/fragmentShader.glsl
+++ src/pages/ImageProcessingMouseEffect/fragmentShader.glsl
@@ -8,20 +8,14 @@ varying vec2 v_texCoord;
 
 uniform float mouseX, mouseY;
 
-vec2 Sine( vec2 point ) {
-    float x = mouseX * 0.028;
-    float y = mouseY * 0.028;
-    return vec2(point.x + x, point.y + y );
-}
 
 void main() {
    vec4 textureCopy = texture2D(u_image1, v_texCoord);
 
-   float x = (mouseX ) * 0.025; 
-   float y = (mouseY ) * 0.025;
+   float x = (mouseX ) * 0.026; 
+   float y = (mouseY ) * 0.026;
 
    vec2 parallax = vec2( x * textureCopy.r, y * textureCopy.r);
 
-//+ vec2(x, y) * textureCopy.r
    gl_FragColor = texture2D(u_image0, v_texCoord + vec2(x, y) * textureCopy.r);
 }
